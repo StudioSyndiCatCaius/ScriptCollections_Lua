b2cef70cbb2df80b7251c085ce7b9f6c6f20fec7
Caius Nelson <jordan.kollins@gmail.com>
2024-04-18 11:00:24 -0500

    added dlg

---
+++ demo/demo.lua
@@ -0,0 +1,7 @@
+G_Dialog=require("sys_dialog")
+EV=G_Dialog.Events
+
+
+local scene={
+    EV.Ln('hello',"How are you today?")
+}
---
+++ src/sys_dialog.lua
@@ -0,0 +1,33 @@
+local c={}
+
+c.Events={
+    Ln=function (speaker,line)
+        return {
+            START=function ()
+                
+            end,
+            END=function ()
+                
+            end,
+        }
+    end
+}
+
+
+
+---@param dialog_tbl table
+c.Start_Dialog=function (dialog_tbl)
+
+    
+end
+
+
+
+
+
+
+
+
+
+
+return c
